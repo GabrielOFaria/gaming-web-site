a5770402fe9c781dbe664760caa9d936ec43185e
Gabriel <gabrielfariaw@gmail.com>
2023-06-26 22:23:47 -0300

    "Placing images and getting text right"

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+'use strict';
+
+// add event on multiple events
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callback)
+    }
+}
+
+// Mobile
+// navbar will  show after clicking menu button 
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNav = function () {
+    navbar.classList.toggle("active");
+    this.classList.toggle("active");
+}
+
+navToggler.addEventListener("click", toggleNav);
+
+const navClose = () => {
+    navbar.classList.remove("active");
+    navToggler.classList.remove("active");
+}
+
+addEventOnElements(navLinks, "click", navClose);
+
+// Header
+// Header will be active after scrolled down to 100px of screen.
+
+const header = document.querySelector("data-header");
+
+const activeEl = function () {
+    if (window.scroll > 100) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
+}
+
+window.addEventListener("scroll", activeEl);
+
+const buttons = document.querySelectorAll("[data-btn]");
+
+const buttonHoverRipple = function (event) {
+    this.style.setProperty("--top", `${event.offsetY}px`);
+    this.style.setProperty("--left", `${event.offsetX}px`);
+}
+
+addEventOnElements(buttons, "mousemove", buttonHoverRipple);
