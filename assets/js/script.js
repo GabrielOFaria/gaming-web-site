f69af3cc376acd37150c678d256b8232f8b67289
Gabriel <gabrielfariaw@gmail.com>
2023-07-01 12:32:13 -0300

    Finalizing  Js

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,15 +32,20 @@ addEventOnElements(navLinks, "click", navClose);
 // Header
 // Header will be active after scrolled down to 100px of screen.
 
-const header = document.querySelector("data-header");
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 const activeEl = function () {
-    if (window.scroll > 100) {
+    if (window.scrollY > 100) {
         header.classList.add("active");
+        backTopBtn.classList.add("active");
     } else {
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
 }
+
+window.addEventListener("scroll", activeEl);
 
 window.addEventListener("scroll", activeEl);
 
@@ -52,3 +57,43 @@ const buttonHoverRipple = function (event) {
 }
 
 addEventOnElements(buttons, "mousemove", buttonHoverRipple);
+
+// Scroll Reveal
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const revealElementOnScroll = function () {
+  for (let i = 0, len = revealElements.length; i < len; i++) {
+    const isElementInsideWindow = revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.1;
+
+    if (isElementInsideWindow) {
+      revealElements[i].classList.add("revealed");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealElementOnScroll);
+
+window.addEventListener("load", revealElementOnScroll);
+
+// Custom cursor
+
+const cursor = document.querySelector("[data-cursor]");
+const hoverElements =[...document.querySelectorAll("a"), ...document.querySelectorAll("button")];
+
+const cursorMove = function(event){
+    cursor.style.top = `${event.clientY}px`;
+    cursor.style.left = `${event.clientX}px`;
+}
+
+window.addEventListener("mousemove", cursorMove);
+
+addEventOnElements(hoverElements, "mouseover", function(){
+    cursor.classList.add("hovered");
+});
+
+addEventOnElements(hoverElements, "mouseout", function(){
+    cursor.classList.remove("hovered");
+});
+
+
